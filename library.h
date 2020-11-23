71f5b84b0744c1eb385eb9a549097f94294b0e85
Andrew Zammetti <azamm235@live.kutztown.edu>
2020-11-23 16:04:27 -0500

    Update library.h

--- library.h
+++ library.h
@@ -1,3 +1,17 @@
+//////////////////////////////////////////////////
+//
+// Names: Tanner Kern
+// Creation Date: November 15, 2020
+// Due Date: November 24, 2020
+// Course: CSC328 - Network Programming
+// Professor Name: Dr. Frye
+// Assignment: Simple Chat Server
+// Filename: library.h
+// Purpose: The header file for the library.
+//          The completed functions are in library.cpp
+//  
+///////////////////////////////////////////////////
+
 #include <string>
 #include <iostream>
 #include <sys/types.h>
