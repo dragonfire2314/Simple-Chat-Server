5c8b4a46de9e8e232f803ca5ad4d26ea85e497ac
Andrew Zammetti <azamm235@live.kutztown.edu>
2020-11-24 12:23:44 -0500

    Update library.h

--- library.h
+++ library.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////
 //
-// Names: Tanner Kern
+// Names: Tanner Kern, Matt Masak, Andrew Zammetti
 // Creation Date: November 15, 2020
 // Due Date: November 24, 2020
 // Course: CSC328 - Network Programming
@@ -12,6 +12,8 @@
 //  
 ///////////////////////////////////////////////////
 
+
+// ALL FUNCTION DOCUMENTATION IS IN library.cpp
 #include <string>
 #include <iostream>
 #include <sys/types.h>
@@ -21,17 +23,13 @@
 #include <string.h>
 #include <vector>
 
-//
-// ALL FUNCTION DOCUMENTATION IS IN LIBRARY.CPP
-// 
-
 struct Socket 
 {
     int socketID;
     struct addrinfo *res;
 };
 
-void setupSocket(std::string ip, Socket *s, bool isServer, char* portnum);
+void setupSocket(std::string ip, Socket *s, bool isServer, std::string portnum);
 
 void sendMessage(int socketID, std::string msg);
 std::string recvMessage(int socketID);
